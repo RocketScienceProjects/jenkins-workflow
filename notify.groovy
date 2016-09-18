e64c8de62d0e85d2553ca8464b82996b9d8b51a7
okram999 <nirish.okram@gmail.com>
2016-09-17 20:02:08 -0500

    changed the extension from .gsh to groovy

---
+++ notify.groovy
@@ -0,0 +1,14 @@
+#!/usr/bin/env groovy
+
+println('Loaded notify.gsh')
+
+def notifyFailed() {
+  emailext (to: "${EMAIL}",
+      subject: "FAILED: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]'",
+      body: "<p>FAILED: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]'</p> <p>Check console output at &quot;<a href='${env.BUILD_URL}'>${env.JOB_NAME} [${env.BUILD_NUMBER}]</a>&quot;</p>",
+        recipientProviders: [[$class: 'CulpritsRecipientProvider']],
+        mimeType:'text/html'
+    )
+}
+
+return this;
