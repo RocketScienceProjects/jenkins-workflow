55e5f749435a43e55e0060041eaac2ae6f7706cb
okram999 <nirish.okram@gmail.com>
2016-09-23 14:48:31 -0500

    adds the build time fetcher

--- jenks_debug.groovy
+++ jenks_debug.groovy
@@ -23,6 +23,14 @@ node('Linux'){
        //def buildtime = currentBuild.getDuration();
        long elapsedMillis = System.currentTimeMillis() - startTime;
        println "This is the build time: ${elapsedMillis}"
+
+/*
+       Date date = new Date(elapsedMillis);
+       DateFormat formatter = new SimpleDateFormat("HH:mm:ss:SSS");
+       String dateFormatted = formatter.format(date);
+
+       println "${dateFormatted}"
+*/
      }
 
     catch(err){
