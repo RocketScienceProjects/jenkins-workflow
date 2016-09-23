70b5ea3b743b93b37aa284663f25b07a1bf86192
okram999 <nirish.okram@gmail.com>
2016-09-23 12:11:56 -0500

    test

--- jenks.groovy
+++ jenks.groovy
@@ -6,7 +6,7 @@ node('Linux'){
       stage name: 'Init'
         //def mvnHome = tool 'M3'
         def mvnHome = tool 'Linux-Maven'
-        env.PATH = "${mvnHome}/bin/mvn;${env.PATH}"
+        env.PATH = "${mvnHome}/bin;${env.PATH}"
 
      stage name: 'Code Checkout'
         checkout([$class: 'GitSCM', branches: [[name: '*/master']], doGenerateSubmoduleConfigurations: false, extensions: [[$class: 'CloneOption', noTags: false, reference: '', shallow: true]], gitTool: 'Linux_Git', submoduleCfg: [], userRemoteConfigs: [[credentialsId: '8c38cbe8-f4e2-4150-918a-0264a28cff73', url: 'git@github.com:RocketScienceProjects/BlueKing.git']]])
