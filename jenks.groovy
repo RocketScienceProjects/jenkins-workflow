3635232ca84c5feba489a98094a28acef94630f2
okram999 <nirish.okram@gmail.com>
2016-09-23 10:46:49 -0500

    debugging

--- jenks.groovy
+++ jenks.groovy
@@ -7,29 +7,30 @@ node('Linux'){
         //def mvnHome = tool 'M3'
         def mvnHome = tool 'Linux-Maven'
 
-    //  stage name: 'Code Checkout'
-    //     //Checkout the source code
-    //     //checkout([$class: 'GitSCM', branches: [[name: '*/master']], doGenerateSubmoduleConfigurations: false, extensions: [[$class: 'CloneOption', noTags: true, reference: '', shallow: true, timeout: 2]], submoduleCfg: [], userRemoteConfigs: [[credentialsId: '8c38cbe8-f4e2-4150-918a-0264a28cff73', url: 'git@github.com:RocketScienceProjects/BlueKing.git']]])
-    //     checkout([$class: 'GitSCM', branches: [[name: '*/master']], doGenerateSubmoduleConfigurations: false, extensions: [[$class: 'CloneOption', noTags: false, reference: '', shallow: true]], gitTool: 'Linux_Git', submoduleCfg: [], userRemoteConfigs: [[credentialsId: '8c38cbe8-f4e2-4150-918a-0264a28cff73', url: 'git@github.com:RocketScienceProjects/BlueKing.git']]])
-    //
-    //     //checkout the jenkins workflow script
-    //     //checkout changelog: false, poll: false, scm: [$class: 'GitSCM', branches: [[name: '*/master']], doGenerateSubmoduleConfigurations: false, extensions: [[$class: 'RelativeTargetDirectory', relativeTargetDir: 'jenks-scripts']], submoduleCfg: [], userRemoteConfigs: [[credentialsId: '8c38cbe8-f4e2-4150-918a-0264a28cff73', url: 'git@github.com:RocketScienceProjects/jenkins-workflow.git']]]
-    //     checkout changelog: false, poll: false, scm: [$class: 'GitSCM', branches: [[name: '*/master']], doGenerateSubmoduleConfigurations: false, extensions: [[$class: 'CloneOption', noTags: true, reference: '', shallow: true], [$class: 'RelativeTargetDirectory', relativeTargetDir: 'jenks-scripts']], gitTool: 'Linux_Git', submoduleCfg: [], userRemoteConfigs: [[credentialsId: '8c38cbe8-f4e2-4150-918a-0264a28cff73', url: 'git@github.com:RocketScienceProjects/jenkins-workflow.git']]]
-    // stage name: 'Load groovy scripts'
-    //     notify = load 'jenks-scripts/notify.groovy'
-    //
-    //  stage name: 'Code Build'
-    //     //bat "${mvnHome}/bin/mvn -B clean deploy"
-    //     sh "${mvnHome}/bin/mvn -B clean deploy"
-    //  stage name: 'Publish Test Data'
-    //     step([$class: 'JUnitResultArchiver', testResults: '**/target/surefire-reports/TEST-*.xml'])
-    //  //stage name: 'Publish Coverage Report'
-    //     //step([$class: 'JacocoPublisher', sourcePattern: 'src/com/blueking/controller/*.java'])
-    //  /*
-    //  stage name: 'Code Coverage Report'
-    //    step([$class: 'JacocoPublisher', sourcePattern: '**/src/**.java'])
-    // */
+     stage name: 'Code Checkout'
+        //Checkout the source code
+        //checkout([$class: 'GitSCM', branches: [[name: '*/master']], doGenerateSubmoduleConfigurations: false, extensions: [[$class: 'CloneOption', noTags: true, reference: '', shallow: true, timeout: 2]], submoduleCfg: [], userRemoteConfigs: [[credentialsId: '8c38cbe8-f4e2-4150-918a-0264a28cff73', url: 'git@github.com:RocketScienceProjects/BlueKing.git']]])
+        checkout([$class: 'GitSCM', branches: [[name: '*/master']], doGenerateSubmoduleConfigurations: false, extensions: [[$class: 'CloneOption', noTags: false, reference: '', shallow: true]], gitTool: 'Linux_Git', submoduleCfg: [], userRemoteConfigs: [[credentialsId: '8c38cbe8-f4e2-4150-918a-0264a28cff73', url: 'git@github.com:RocketScienceProjects/BlueKing.git']]])
 
+        //checkout the jenkins workflow script
+        //checkout changelog: false, poll: false, scm: [$class: 'GitSCM', branches: [[name: '*/master']], doGenerateSubmoduleConfigurations: false, extensions: [[$class: 'RelativeTargetDirectory', relativeTargetDir: 'jenks-scripts']], submoduleCfg: [], userRemoteConfigs: [[credentialsId: '8c38cbe8-f4e2-4150-918a-0264a28cff73', url: 'git@github.com:RocketScienceProjects/jenkins-workflow.git']]]
+        checkout changelog: false, poll: false, scm: [$class: 'GitSCM', branches: [[name: '*/master']], doGenerateSubmoduleConfigurations: false, extensions: [[$class: 'CloneOption', noTags: true, reference: '', shallow: true], [$class: 'RelativeTargetDirectory', relativeTargetDir: 'jenks-scripts']], gitTool: 'Linux_Git', submoduleCfg: [], userRemoteConfigs: [[credentialsId: '8c38cbe8-f4e2-4150-918a-0264a28cff73', url: 'git@github.com:RocketScienceProjects/jenkins-workflow.git']]]
+    stage name: 'Load groovy scripts'
+        notify = load 'jenks-scripts/notify.groovy'
+
+     stage name: 'Code Build'
+        //bat "${mvnHome}/bin/mvn -B clean deploy"
+        sh "${mvnHome}/bin/mvn -B clean deploy"
+     stage name: 'Publish Test Data'
+        step([$class: 'JUnitResultArchiver', testResults: '**/target/surefire-reports/TEST-*.xml'])
+     //stage name: 'Publish Coverage Report'
+        //step([$class: 'JacocoPublisher', sourcePattern: 'src/com/blueking/controller/*.java'])
+     /*
+     stage name: 'Code Coverage Report'
+       step([$class: 'JacocoPublisher', sourcePattern: '**/src/**.java'])
+    */
+
+    sh 'echo "This is the build time: "'
     println currentBuild.getDuration()
     }
     catch(e){
